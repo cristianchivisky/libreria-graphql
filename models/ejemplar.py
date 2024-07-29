209ce7274222b1a4e2af254a320e1ec653d861a8
Cristian Chivisky <cristian.chivisky@gmail.com>
2024-07-28 22:01:16 -0300

    algunas correcciones

--- models/ejemplar.py
+++ models/ejemplar.py
@@ -3,10 +3,10 @@ from api_config import db
 class Ejemplar(db.Model):
     __tablename__ = "ejemplar"
     isbn = db.Column(db.String(13), primary_key=True)
-    precio = db.Column(db.Numeric(10, 2), nullable=False, check_constraint='precio > 0')
-    stock = db.Column(db.Integer, nullable=False, check_constraint='stock >= 0')
+    precio = db.Column(db.Numeric(10, 2), nullable=False)
+    stock = db.Column(db.Integer, nullable=False)
     dimensiones = db.Column(db.String(225), nullable=False)
-    paginas = db.Column(db.Integer, nullable=False, check_constraint='paginas > 0')
+    paginas = db.Column(db.Integer, nullable=False)
     
     id_libro = db.Column(db.Integer, db.ForeignKey('libro.id_libro'), nullable=False)
     id_editorial = db.Column(db.Integer, db.ForeignKey('editorial.id_editorial'), nullable=False)
